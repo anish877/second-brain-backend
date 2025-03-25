f0113ffa0cf6f09949183ef26540c97a1cc77788
Anish Suman <62011810+anish877@users.noreply.github.com>
2025-03-25 12:08:12 +0530

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -46,7 +46,7 @@ run()
 app.use(e.json())
 app.use(cookieParser())
 app.use(cors({
-    origin: "https://second-brain-rosy.vercel.app",
+    origin: "*",
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization', 'Cookie']
